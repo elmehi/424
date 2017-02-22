b76c2ef3ba32204e378edf8229360f4eabbdd90a
Elliot Hirsch <ehirsch@princeton.edu>
2017-02-21 20:18:44 -0500

    add bernouli

--- classify.py
+++ classify.py
@@ -1,19 +1,23 @@
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import TfidfTransformer
+# from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import BernoulliNB
 import numpy as np
+
+folds =3
 
 fvs = np.loadtxt("data/out_bag_of_words_5.csv", delimiter=',')
 target = np.loadtxt("data/out_classes_5.txt")
-print(fvs[1:10])
 
 ## tfidf does nothing
-transformer = TfidfTransformer()
-fvs_tfidf = transformer.fit_transform(fvs)
+# transformer = TfidfTransformer()
+# fvs_tfidf = transformer.fit_transform(fvs)
 # print(fvs_tfidf.shape)
-print(fvs_tfidf[1:10])
-clf = MultinomialNB() #.fit(fvs, target)
 
+clfmnb = MultinomialNB() #.fit(fvs, target)
+scores = cross_val_score(clfmnb, fvs, target, cv=folds)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
-scores = cross_val_score(clf, fvs_tfidf, target, cv=5)
+clfbnb = BernoulliNB(binarize=0.5) #.fit(fvs, target)
+scores = cross_val_score(clfbnb, fvs, target, cv=folds)
 print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
